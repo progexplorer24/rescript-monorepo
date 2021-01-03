71ed03c19671bdb87402416db0c2a1c09dbec0bb
Prog Explorer <progexplorer@gmail.com>
2021-01-03 22:13:13 +0100

    feat: extend tailwind default config

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,42 @@ module.exports = {
       screens: {
         "omnifood-sm": "481px",
         "omnifood-md": "767px",
+      },
+
+      spacing: {
+        68: '17rem',
+        76: '19rem',
+        84: '21rem',
+        88: '22rem',
+        92: '23rem',
+        100: '25rem',
+        104: '26rem',
+        108: '27rem',
+        112: '28rem',
+        116: '29rem',
+        120: '30rem',
+        124: '31rem',
+        128: '32rem',
+        132: '33rem',
+        136: '34rem',
+        140: '35rem',
+        144: '36rem',
+        148: '37rem',
+        152: '38rem',
+        156: '39rem',
+        160: '40rem',
+        164: '41rem',
+        168: '42rem',
+        172: '43rem',
+        176: '44rem',
+        180: '45rem',
+        184: '46rem',
+        188: '47rem',
+        192: '48rem',
+        196: '49rem',
+        200: '50rem',
       }
+
     },
      /* We override the default font-families with our own default prefs  */
      fontFamily: {
@@ -42,5 +77,8 @@ module.exports = {
     },
     },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    // ...
+  ],
 }
