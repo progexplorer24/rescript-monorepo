cd3224f07548c15e2f692b37faa60566d920a219
Progexplorer <progexplorer@gmail.com>
2021-05-09 11:00:47 +0200

    build: update next config to webpack5

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const bsconfig = require('./bsconfig.json');
+const fs = require("fs");
 
 // NOTE: https://forum.rescript-lang.org/t/purpose-alternatives-to-next-transpile-modules-in-next-rescript-projects/827/5
 const transpileModules = ["bs-platform"].concat(bsconfig["bs-dependencies"]);
@@ -6,6 +7,26 @@ const withTM = require("next-transpile-modules")(transpileModules);
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 })
+
+// There is an issue where webpack doesn't detect npm packages within node_modules when
+// there is no dedicated package.json "main" entry + index.js file existent.
+// This function will make sure that every ReScript dependency folder is conforming
+// to webpack's resolve mechanism
+//
+// This will eventually be removed at some point, so keep an eye out for updates
+// on our template repository.
+function patchResDeps() {
+  ["bs-platform"].concat(bsconfig["bs-dependencies"]).forEach((bsDep) => {
+    fs.writeFileSync(`./node_modules/${bsDep}/index.js`, "");
+    const json = require(`./node_modules/${bsDep}/package.json`);
+    json.main = "index.js";
+    fs.writeFileSync(
+      `./node_modules/${bsDep}/package.json`,
+      JSON.stringify(json, null, 2)
+    );
+  });
+}
+patchResDeps(); // update package.json and create empty `index.js` before transpiling
 
 const config = {
   target: "serverless",
@@ -19,12 +40,30 @@ const config = {
     if (!isServer) {
       // We shim fs for things like the blog slugs component
       // where we need fs access in the server-side part
-      config.node = {
-        fs: 'empty'
-      }
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+      };
     }
+
+    config.module.rules.push({
+      test: /\.m?js$/,
+      use: options.defaultLoaders.babel,
+      exclude: /node_modules/,
+      type: "javascript/auto",
+      resolve: {
+        fullySpecified: false,
+      }
+    });
+
     return config
-  }
+  },
+  future: {
+    webpack5: true
+  },
+  images: {
+    domains: ['vercel.com'],
+  },
 };
 
 module.exports = withMDX(withTM(config));
