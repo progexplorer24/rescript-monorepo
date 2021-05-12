1d5e7ee394c626e17f5131fc94544912dd5d978f
Prog Explorer <progexplorer@gmail.com>
2021-05-12 22:19:33 +0200

    build: update rescript module

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const bsconfig = require('./bsconfig.json');
 const fs = require("fs");
 
 // NOTE: https://forum.rescript-lang.org/t/purpose-alternatives-to-next-transpile-modules-in-next-rescript-projects/827/5
-const transpileModules = ["bs-platform"].concat(bsconfig["bs-dependencies"]);
+const transpileModules = ["rescript"].concat(bsconfig["bs-dependencies"]);
 const withTM = require("next-transpile-modules")(transpileModules);
 // const withMDX = require('@next/mdx')({
 //   extension: /\.mdx?$/,
@@ -16,7 +16,7 @@ const withTM = require("next-transpile-modules")(transpileModules);
 // This will eventually be removed at some point, so keep an eye out for updates
 // on our template repository.
 function patchResDeps() {
-  ["bs-platform"].concat(bsconfig["bs-dependencies"]).forEach((bsDep) => {
+  ["rescript"].concat(bsconfig["bs-dependencies"]).forEach((bsDep) => {
     fs.writeFileSync(`./node_modules/${bsDep}/index.js`, "");
     const json = require(`./node_modules/${bsDep}/package.json`);
     json.main = "index.js";
@@ -28,22 +28,30 @@ function patchResDeps() {
 }
 patchResDeps(); // update package.json and create empty `index.js` before transpiling
 
+const isWebpack5 = true;
+
 const config = {
   target: "serverless",
   pageExtensions: ["jsx", "js", "bs.js", "mdx"],
-  transpileModules: ["bs-platform"].concat(bsconfig["bs-dependencies"]),
   env: {
     ENV: process.env.NODE_ENV,
   },
   webpack: (config, options) => {
     const { isServer } = options;
-    if (!isServer) {
-      // We shim fs for things like the blog slugs component
-      // where we need fs access in the server-side part
-      config.resolve.fallback = {
-        fs: false,
-        path: false,
-      };
+
+    if (isWebpack5) {
+
+      if (!isServer) {
+        // We shim fs for things like the blog slugs component
+        // where we need fs access in the server-side part
+        config.resolve.fallback = {
+          fs: false,
+          path: false,
+        };
+
+    }
+
+    
     }
 
     config.module.rules.push({
