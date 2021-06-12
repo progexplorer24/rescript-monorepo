d534a8fc075a4befbc4e64136f89969c9037a87a
Prog Explorer <progexplorer@gmail.com>
2021-06-12 11:21:10 +0200

    feat: create empty NextThemes file

--- src/components/blog/Logo.mjs
+++ src/components/blog/Logo.mjs
@@ -3,7 +3,10 @@
 import * as React from "react";
 import * as Tailwind$RescriptMonorepo from "../../styles/tailwind/Tailwind.mjs";
 
-var elementStyles = Tailwind$RescriptMonorepo.twStyle([]);
+var elementStyles = Tailwind$RescriptMonorepo.twStyle([
+      Tailwind$RescriptMonorepo.w("v8"),
+      Tailwind$RescriptMonorepo.h("v8")
+    ]);
 
 var Styles = {
   elementStyles: elementStyles
@@ -11,7 +14,11 @@ var Styles = {
 
 function Logo(Props) {
   var classNameOpt = Props.className;
+  var colorOneOpt = Props.colorOne;
+  var colorTwoOpt = Props.colorTwo;
   var className = classNameOpt !== undefined ? classNameOpt : "";
+  var colorOne = colorOneOpt !== undefined ? colorOneOpt : "#2563EB";
+  var colorTwo = colorTwoOpt !== undefined ? colorTwoOpt : "#DBEAFE";
   return React.createElement("svg", {
               className: Tailwind$RescriptMonorepo.merge([
                     elementStyles,
@@ -26,20 +33,20 @@ function Logo(Props) {
             }, React.createElement("circle", {
                   cx: "256",
                   cy: "256",
-                  fill: "#F8F6F8",
+                  fill: colorOne,
                   r: "256"
                 }), React.createElement("path", {
                   d: "M255.437,512c-70.693,0-128-57.307-128-128s57.307-128,128-128s128-57.307,128-128\n\ts-57.307-128-128-128H256c141.385,0,256,114.615,256,256S397.385,512,256,512H255.437z",
-                  fill: "#3E3B43"
+                  fill: colorTwo
                 }), React.createElement("circle", {
                   cx: "256",
                   cy: "384",
-                  fill: "#F8F6F8",
+                  fill: colorOne,
                   r: "28.473"
                 }), React.createElement("circle", {
                   cx: "256",
                   cy: "139.4",
-                  fill: "#3E3B43",
+                  fill: colorTwo,
                   r: "28.473"
                 }), React.createElement("g", undefined), React.createElement("g", undefined), React.createElement("g", undefined), React.createElement("g", undefined), React.createElement("g", undefined), React.createElement("g", undefined), React.createElement("g", undefined), React.createElement("g", undefined), React.createElement("g", undefined), React.createElement("g", undefined), React.createElement("g", undefined), React.createElement("g", undefined), React.createElement("g", undefined), React.createElement("g", undefined), React.createElement("g", undefined));
 }
--- src/components/blog/Logo.res
+++ src/components/blog/Logo.res
@@ -1,10 +1,10 @@
 module Styles = {
   open Tailwind
-  let elementStyles = twStyle([])
+  let elementStyles = twStyle([w(#v8), h(#v8)])
 }
 
 @react.component
-let make = (~className="") =>
+let make = (~className="", ~colorOne="#2563EB", ~colorTwo="#DBEAFE") =>
   <svg
     version="1.1"
     id="Layer_1"
@@ -13,14 +13,14 @@ let make = (~className="") =>
     y="0px"
     viewBox="0 0 512 512"
     className={Tailwind.merge(. [Styles.elementStyles, className])}>
-    <circle fill="#F8F6F8" cx="256" cy="256" r="256" />
+    <circle fill=colorOne cx="256" cy="256" r="256" />
     <path
-      fill="#3E3B43"
+      fill=colorTwo
       d="M255.437,512c-70.693,0-128-57.307-128-128s57.307-128,128-128s128-57.307,128-128
 	s-57.307-128-128-128H256c141.385,0,256,114.615,256,256S397.385,512,256,512H255.437z"
     />
-    <circle fill="#F8F6F8" cx="256" cy="384" r="28.473" />
-    <circle fill="#3E3B43" cx="256" cy="139.4" r="28.473" />
+    <circle fill=colorOne cx="256" cy="384" r="28.473" />
+    <circle fill=colorTwo cx="256" cy="139.4" r="28.473" />
     <g />
     <g />
     <g />
