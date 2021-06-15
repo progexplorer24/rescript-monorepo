bbac241af9d61121430cf4fde2202d26cbbbd17f
Prog Explorer <progexplorer@gmail.com>
2021-06-15 09:33:33 +0200

    build: remove depPatch hack from next config

--- next.config.js
+++ next.config.js
@@ -4,35 +4,11 @@ const fs = require("fs");
 // NOTE: https://forum.rescript-lang.org/t/purpose-alternatives-to-next-transpile-modules-in-next-rescript-projects/827/5
 const transpileModules = ["rescript"].concat(bsconfig["bs-dependencies"]);
 const withTM = require("next-transpile-modules")(transpileModules);
-// const withMDX = require('@next/mdx')({
-//   extension: /\.mdx?$/,
-// })
-
-// There is an issue where webpack doesn't detect npm packages within node_modules when
-// there is no dedicated package.json "main" entry + index.js file existent.
-// This function will make sure that every ReScript dependency folder is conforming
-// to webpack's resolve mechanism
-//
-// This will eventually be removed at some point, so keep an eye out for updates
-// on our template repository.
-function patchResDeps() {
-  ["rescript"].concat(bsconfig["bs-dependencies"]).forEach((bsDep) => {
-    fs.writeFileSync(`./node_modules/${bsDep}/index.js`, "");
-    const json = require(`./node_modules/${bsDep}/package.json`);
-    json.main = "index.js";
-    fs.writeFileSync(
-      `./node_modules/${bsDep}/package.json`,
-      JSON.stringify(json, null, 2)
-    );
-  });
-}
-patchResDeps(); // update package.json and create empty `index.js` before transpiling
-
 const isWebpack5 = true;
 
 const config = {
   target: "serverless",
-  pageExtensions: ["jsx", "js", "bs.js", "mdx"],
+  pageExtensions: ["jsx", "js", "mdx"],
   env: {
     ENV: process.env.NODE_ENV,
   },
