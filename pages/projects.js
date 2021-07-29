2aad060a21d6a4227693799bf49376dd61830c37
Prog Explorer <progexplorer@gmail.com>
2021-07-29 13:48:29 +0200

    feat: add projects page

---
+++ pages/projects.js
@@ -0,0 +1,13 @@
+import { default as ProjectsPage } from "../src/Projects.mjs";
+
+// Note:
+// We need to wrap the make call with
+// a Fast-Refresh conform function name,
+// (in this case, uppercased first letter)
+//
+// If you don't do this, your Fast-Refresh will
+// not work!
+
+export default function Projects(props) {
+  return <ProjectsPage {...props}/>;
+}
